5a8957eab02b36b8f39008d65ab7c5f03a744f8c
Feyfa <fisikamodern00@gmail.com>
2024-07-13 00:34:25 +0700

    - ubah baseurl axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  // baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'http://sekolah.backend.com/api',
   timeout: 30000
 });
 
